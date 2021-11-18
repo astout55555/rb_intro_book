a268a71ee8341e4435d9b3d0bc6ee8e6e8d36a67
astout55555 <alex.the.stout5@gmail.com>
2021-11-18 13:12:50 -0700

    Finished the exercises for 'The Basics' chapter.

---
+++ the_basics/ex_3.rb
@@ -0,0 +1,8 @@
+horror_classics = {
+  "The Thing": 1982,
+  "Invasion of the Body Snatchers": 1978,
+  "The Haunting": 1963,
+  "Psycho": 1960
+}
+
+horror_classics.each{ |title, date| puts date }
---
+++ the_basics/ex_4.rb
@@ -0,0 +1,5 @@
+release_dates = [1982, 1978, 1963, 1960]
+
+puts release_dates
+
+#Solution shows "puts dates[0]" etc, but this seems to be the same result
