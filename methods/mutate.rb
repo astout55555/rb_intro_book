e2e05427a9a51638eaf501d6f57338b99c7e19a4
astout55555 <alex.the.stout5@gmail.com>
2022-01-05 18:47:49 -0700

    Made some adjustments to the mutate.rb file during review to clarify things in the notes.

--- methods/mutate.rb
+++ methods/mutate.rb
@@ -22,10 +22,10 @@ p "Before no_mutate method: #{a}"
 no_mutate(a)
 p "After no_mutate method: #{a}"
 
-#Unfortunately, can only memorize which methods mutate the caller and which don't.
-#Can look at the documation or repeatedly use it enough to lock it in memory.
+# Unfortunately, can only memorize which methods mutate the caller and which don't.
+# Can look at the documation or repeatedly use it enough to lock it in memory.
 
-# puts vs return: The Sequel
+# puts vs return: The Sequel - Ruby always returns the evaluated result of the last line of an expression (unless `return` comes before it)
 
 a = [1, 2, 3]
 
@@ -34,6 +34,5 @@ def mutate(array)
 end
 
 p "Before mutate method: #{a}"
-p mutate(a)
-p "after mutate method: #{a}"
-
+p mutate(a) # Prints 3 to the console, the value returned by the method. The method was still called, so it still mutates the array!
+p "After mutate method: #{a}" # Prints [1, 2] since the array was mutated.
